f08510665fc009a93227688b2a42c95707bc3d4e
Lucas Remígio <lucasxavier5435@hotmail.com>
2025-10-15 18:44:33 +0100

    add: ipynb notebook for easier code

--- 4.save_lora_merged_model.py
+++ 4.save_lora_merged_model.py
@@ -1,13 +1,13 @@
-# convert_to_gguf.py
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
 import torch
 
 print("Loading your finetuned model...")
 base_model = AutoModelForCausalLM.from_pretrained(
-    "dgoogle/gemma-3n-E2B-it",
-    torch_dtype=torch.float16,
+    "google/gemma-3n-E2B-it",
+    dtype=torch.float16,
 )
+
 model = PeftModel.from_pretrained(base_model, "outputs/lora")
 model = model.merge_and_unload()
 
